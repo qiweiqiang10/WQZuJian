b710a8e2fd578ba5375b0c67f4741b67b0c7cb36
qiweiqiang <qiweiqiang@people.cn>
2020-01-08 15:29:12 +0800

    init project

---
+++ WQZuJian/Classes/Person.h
@@ -0,0 +1,21 @@
+//
+//  Person.h
+//  TestForToday
+//
+//  Created by 人民网 on 2019/10/21.
+//  Copyright © 2019 人民网. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@property(nonatomic,assign) int age;
+
+-(void)sayAwordHell;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WQZuJian/Classes/Person.m
@@ -0,0 +1,22 @@
+//
+//  Person.m
+//  TestForToday
+//
+//  Created by 人民网 on 2019/10/21.
+//  Copyright © 2019 人民网. All rights reserved.
+//
+
+#import "Person.h"
+@implementation Person
+
+- (void)dealloc
+{
+//    NSLog(@"Person - dealloc");
+}
+
+-(void)sayAwordHell
+{
+    NSLog(@"Hello++++++3333333++++-Hello");
+}
+
+@end
